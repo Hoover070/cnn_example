7d2ca35a3c4fe0937c1644c261c74c848483df27
Hoover070 <willhooverfilm@gmail.com>
2024-01-18 19:33:03 -0500

    test_CNN runable

--- cnn_example.py
+++ cnn_example.py
@@ -147,22 +147,37 @@ def compile_model(model):
 def data_aug():
     data_generator = ImageDataGenerator(width_shift_range=0.2,
                                         height_shift_range=0.1,
-                                        zoom_range=[0.85, 1.2],
-                                        rotation_range=15,
+                                        zoom_range=[0.90, 1.1],
+                                        rotation_range=10,
                                         horizontal_flip=True,
                                         fill_mode='nearest')
     return data_generator
 
 # harness
-X_train, y_test, x_test, y_train = load_cifar()
-X_train, x_test, y_train, y_test = preprocess(X_train, y_train, x_test, y_test)
+X_train, y_train, X_test, y_test = load_cifar()
+X_train, X_test, y_train, y_test = preprocess(X_train, y_train, X_test, y_test)
 model = define_model(X_train, y_train)
 compile_model(model)
-model.summary()
+#model.summary()
+
+
+train_gen = data_aug()
+'''data = np.expand_dims(X_train[60], axis=0)
+print(data.shape)
+train_generator = train_gen.flow(data, batch_size=1)
+plot_iter(train_generator)
+plot_imgs(X_train)'''
 
 # fit model
+train_generator = train_gen.flow(X_train, y_train, batch_size=BATCH_SIZE)
+steps_per_epoch = X_train.shape[0] // BATCH_SIZE
+history = model.fit(train_generator, validation_data=[X_test, y_test],
+                     steps_per_epoch=steps_per_epoch,
+                     epochs=5)
 
 # plot learning curves
+plot_scores(history)
 
 
 # test accuracy
+_, acc = model.evaluate(X_test, y_test)
